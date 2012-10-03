ad72ec91293948b0f53af53e4990e21ee2398487
Will Farrington <wfarr@github.com>
2012-10-02 17:09:15 -0700

    installed is default ensure for pkgs

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,5 @@
 class xquartz {
   package { 'XQuartz':
-    ensure   => installed,
     provider => 'pkgdmg',
     source   => 'http://static.macosforge.org/xquartz/downloads/SL/XQuartz-2.7.2.dmg'
   }
