5f1b07b52eba21540fe757402cbc5234da0aedaf
Naoya Nakazawa <naoya.n@gmail.com>
2016-09-14 17:53:44 +0900

    Bumpup 2.7.9

--- spec/classes/xquartz_spec.rb
+++ spec/classes/xquartz_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe 'xquartz' do
-  let(:params) { {:version => '2.7.7'} }
+  let(:params) { {:version => '2.7.9'} }
 
   it do
     should contain_package('XQuartz').with({
       :provider => 'pkgdmg',
-      :source   => 'http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.7.dmg'
+      :source   => 'http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.9.dmg'
     })
   end
 end
